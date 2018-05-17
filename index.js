84b38e14b53b24087ca42dfcdbbdcb75f9e2cd95
Julian Ronayi <yuliy.ronayi@azinec.com>
2018-05-17 17:35:33 +0300

    Add cron to run job with data

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ require('dotenv').config();
 // Core modules
 const http = require('http');
 const fs = require('fs');
+// Jobs
+const { CronJob } = require('cron');
 // Utils
 const synchWithTrello = require('./utils/trello');
 const returnHTMLtempate = require('./public/index');
-const say = require('say');
 const {FILE_NAME} = require('./config');
-synchWithTrello();
+// Start job before daily standup
+const syncTrelloJob = new CronJob({
+  cronTime: '00 00 10 * * *',
+  onTick: synchWithTrello,
+  start: true,
+  timeZone: 'Europe/Kiev'
+});
+
+syncTrelloJob.start();
 // Server
 http.createServer(function (req, res) {
   fs.readFile(FILE_NAME, function (err, data) {
