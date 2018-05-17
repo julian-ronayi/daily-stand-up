50d27a402b86dd1b5100d1ab64c16cfec53e26f9
Julian Ronayi <yuliy.ronayi@azinec.com>
2018-05-17 17:52:17 +0300

    Add manual update

--- index.js
+++ index.js
@@ -2,29 +2,27 @@ require('dotenv').config();
 // Core modules
 const http = require('http');
 const fs = require('fs');
-// Jobs
-const { CronJob } = require('cron');
 // Utils
 const synchWithTrello = require('./utils/trello');
 const returnHTMLtempate = require('./public/index');
 const {FILE_NAME} = require('./config');
 // Start job before daily standup
-const syncTrelloJob = new CronJob({
-  cronTime: '00 00 10 * * *',
-  onTick: synchWithTrello,
-  start: true,
-  timeZone: 'Europe/Kiev'
-});
-
-syncTrelloJob.start();
 // Server
 http.createServer(function (req, res) {
+  if (req.url === '/update') {
+    synchWithTrello();
+    res.end('success');
+  };
+
   fs.readFile(FILE_NAME, function (err, data) {
-    if (err) throw err;
+    if (err) {
+      res.end('No file');
+    } else {
+      const parsedData = JSON.parse(data);
+      const template = returnHTMLtempate(parsedData.content);
 
-    const parsedData = JSON.parse(data);
-    const template = returnHTMLtempate(parsedData.content);
+      res.end(template);
+    }
 
-    res.end(template);
   })
 }).listen(3000, () => console.log('Listening port 3000'));
