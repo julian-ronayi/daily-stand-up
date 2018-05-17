d0cd2f166f766fca7336c2b097a63e844f273b79
Julian Ronayi <yuliy.ronayi@azinec.com>
2018-05-17 18:08:30 +0300

    Move voice library to the bottom

--- public/index.js
+++ public/index.js
@@ -41,7 +41,6 @@ const returnHTMLtempate = (content) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>What Julian did yesterday</title>
-        <script src="http://code.responsivevoice.org/responsivevoice.js"></script>
       </head>
       ${STYLES}
       <body>
@@ -58,6 +57,7 @@ const returnHTMLtempate = (content) => {
             </p>
           </div>
         </div>
+        <script src="http://code.responsivevoice.org/responsivevoice.js"></script>
         <script type="text/javascript">
           window.onload = function () {
             responsiveVoice.speak("${content.replace(/<\/?br\/?>|\"/g, '')}", "UK English Male", {pitch: 0.9, rate: 0.9});
