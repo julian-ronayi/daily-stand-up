28a9305239da78d1b14b10b6165d8ca19b52e1ad
Julian Ronayi <yuliy.ronayi@azinec.com>
2018-05-17 15:36:32 +0300

    Add html templates

--- index.js
+++ index.js
@@ -74,63 +74,8 @@
 //       })
 //   });
 const http = require('http');
+const returnHTMLtempate = require('./public/index');
 
 http.createServer(function (req, res) {
-  res.end(`<!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-
-    <style>
-      body {
-        background-color: #eee;
-        color: #494949;
-        font-family: Arial, sans-serif;
-      }
-      .main {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        width: 100%;
-        height: 100%;
-      }
-      .title, .content, .profilePic {
-        padding: 2rem;
-      }
-      .title {
-        text-align: center;
-        text-transform: uppercase;
-        font-weight: 200;
-      }
-      .content {
-        font-size: 1.35em;
-      }
-      .profilePic img {
-        display: block;
-        border-radius: 50%;
-        border: 1px solid black;
-      }
-    </style>
-  </head>
-  <body>
-    <div class="main">
-      <div class="title">
-        <h1>Hello, my name is Julian</h1>
-      </div>
-      <div class="profilePic">
-        <img src="https://avatars0.githubusercontent.com/u/2480604?s=460&v=4"/>
-      </div>
-      <div class="content">
-        <p>
-          Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться. Lorem Ipsum используют потому, что тот обеспечивает более или менее стандартное заполнение шаблона, а также реальное распределение букв и пробелов в абзацах, которое не получается при простой дубликации "Здесь ваш текст.. Здесь ваш текст.. Здесь ваш текст.." Многие программы электронной вёрстки и редакторы HTML используют Lorem Ipsum в качестве текста по умолчанию, так что поиск по ключевым словам "lorem ipsum" сразу показывает, как много веб-страниц всё ещё дожидаются своего настоящего рождения. За прошедшие годы текст Lorem Ipsum получил много версий. Некоторые версии появились по ошибке, некоторые - намеренно (например, юмористические варианты).
-        </p>
-      </div>
-    </div>
-  </body>
-  </html>
-`)
-}).listen(3000);
+  res.end(returnHTMLtempate('Some content'))
+}).listen(3000, () => console.log('Listening port 3000'));
---
+++ public/index.js
@@ -0,0 +1,66 @@
+const STYLES = `
+  <style>
+    body {
+      background-color: #eee;
+      color: #494949;
+      font-family: Arial, sans-serif;
+    }
+    .main {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      width: 100%;
+      height: 100%;
+    }
+    .title, .content, .profilePic {
+      padding: 2rem;
+    }
+    .title {
+      text-align: center;
+      text-transform: uppercase;
+      font-weight: 200;
+    }
+    .content {
+      font-size: 1.35em;
+    }
+    .profilePic img {
+      display: block;
+      border-radius: 50%;
+      border: 1px solid black;
+    }
+  </style>
+`;
+
+const returnHTMLtempate = (content) => {
+  return (
+    `<!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>What Julian did yesterday</title>
+      </head>
+      ${STYLES}
+      <body>
+        <div class="main">
+          <div class="title">
+            <h1>Hello, my name is Julian</h1>
+          </div>
+          <div class="profilePic">
+            <img src="https://avatars0.githubusercontent.com/u/2480604?s=460&v=4"/>
+          </div>
+          <div class="content">
+            <p>
+              ${content}
+            </p>
+          </div>
+        </div>
+      </body>
+      </html>
+    `
+  )
+};
+
+module.exports = returnHTMLtempate;
